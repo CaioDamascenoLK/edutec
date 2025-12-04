143431ddf18756d2d4f8143a01c98ec74bf24e96
lklordsjoga <lklordsjoga@gmail.com>
2025-12-04 01:04:17 +0000

    porra viu

--- frontend/pages/ranking/ranking.js
+++ frontend/pages/ranking/ranking.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const rankingList = document.getElementById("ranking-list");
 
     const backendUrl = 'https://edutec-pied.vercel.app';
-    const user = JSON.parse(localStorage.getItem('user'))
+    const user = localStorage.getItem('user')
 
     if (user)
     fetch(`${backendUrl}/ranking`)
